d55e6c7e83383d36bd1e1c025e23938271a6a060
Camilo <camilo.orbes@udea.edu.co>
2018-03-17 14:28:29 -0500

    Probblema 3

---
+++ Problema 3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int compara_pala(char*str1, char*str2);
+
+int main()
+{
+    char str1[]={"Palabra"};
+    char str2[]={"palabra"};
+    compara_pala(str1,str2);
+
+}
+
+int compara_pala(char *str1, char *str2){
+    if(str1==str2){
+        cout<<"Son iguales "<<endl;
+    }
+    else{
+        cout<<"Son diferentes "<<endl;
+}
+}
+
